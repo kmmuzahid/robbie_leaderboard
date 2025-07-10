a53f6beaa37c5e466c00334d74cceb30c1a41bad
aurnabbarua409 <aurnabbarua409@gmail.com>
2025-07-10 18:30:32 +0600

    updated

--- lib/screens/auth_screens/login_screen/login_screen.dart
+++ lib/screens/auth_screens/login_screen/login_screen.dart
@@ -31,143 +31,145 @@ class _LoginScreenState extends State<LoginScreen> {
         ),
         child: Scaffold(
           backgroundColor: AppColors.blueDark,
-          body: Center(
-            child: controller.isLoading.value
-                ? const CircularProgressIndicator.adaptive()
-                : SingleChildScrollView(
-                    padding: const EdgeInsets.symmetric(horizontal: 20),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        const Center(
-                          child: TextWidget(
-                            text: AppStrings.welcome,
-                            fontColor: AppColors.blueLighter,
-                            fontWeight: FontWeight.w600,
-                            fontSize: 32,
-                          ),
-                        ),
-                        const SpaceWidget(spaceHeight: 12),
-                        const Center(
-                          child: TextWidget(
-                            text: AppStrings.enterPersonalData,
-                            fontColor: AppColors.blueLighter,
-                            fontWeight: FontWeight.w400,
-                            fontSize: 14,
-                          ),
-                        ),
-                        const SpaceWidget(spaceHeight: 28),
-                        const Padding(
-                          padding: EdgeInsets.symmetric(horizontal: 16),
-                          child: TextWidget(
-                            text: AppStrings.email,
-                            fontColor: AppColors.greyDark,
-                            fontWeight: FontWeight.w400,
-                            fontSize: 16,
-                          ),
-                        ),
-                        const SpaceWidget(spaceHeight: 8),
-                        TextFieldWidget(
-                          controller: controller.emailController,
-                          hintText: 'Enter email',
-                          maxLines: 1,
-                        ),
-                        const SpaceWidget(spaceHeight: 16),
-                        const Padding(
-                          padding: EdgeInsets.symmetric(horizontal: 16),
-                          child: TextWidget(
-                            text: AppStrings.password,
-                            fontColor: AppColors.greyDark,
-                            fontWeight: FontWeight.w400,
-                            fontSize: 16,
-                          ),
-                        ),
-                        const SpaceWidget(spaceHeight: 8),
-                        TextFieldWidget(
-                          controller: controller.passwordController,
-                          hintText: 'Enter password',
-                          maxLines: 1,
-                          suffixIcon: AppIconPath.visibilityOnIcon,
-                        ),
-                        const SpaceWidget(spaceHeight: 24),
-                        // Add Remember Me Checkbox
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Row(
-                              children: [
-                                SizedBox(
-                                  width: 24,
-                                  height: 24,
-                                  child: Transform.scale(
-                                    scale: 0.9,
-                                    child: Checkbox(
-                                      value: controller.rememberMe.value,
-                                      onChanged: (value) {
-                                        controller.rememberMe.value =
-                                            value ?? false;
-                                      },
-                                      activeColor: AppColors.blueLighter,
-                                      checkColor: AppColors.blueDark,
-                                    ),
-                                  ),
-                                ),
-                                const SpaceWidget(spaceWidth: 8),
-                                const TextWidget(
-                                  text: AppStrings.rememberMe,
-                                  fontColor: AppColors.blueLighter,
-                                  fontWeight: FontWeight.w400,
-                                  fontSize: 12,
-                                ),
-                              ],
+          body: Obx(
+            () => Center(
+              child: controller.isLoading.value
+                  ? const CircularProgressIndicator.adaptive()
+                  : SingleChildScrollView(
+                      padding: const EdgeInsets.symmetric(horizontal: 20),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const Center(
+                            child: TextWidget(
+                              text: AppStrings.welcome,
+                              fontColor: AppColors.blueLighter,
+                              fontWeight: FontWeight.w600,
+                              fontSize: 32,
                             ),
-                            TextButtonWidget(
-                              onPressed: () {
-                                Get.toNamed(AppRoutes.forgotPasswordScreen);
-                              },
-                              text: AppStrings.forgotPassword,
-                              textColor: AppColors.skyBlue,
-                              fontSize: 12,
-                              fontWeight: FontWeight.w500,
-                              decoration: TextDecoration.underline,
-                              decorationColor: AppColors.skyBlue,
-                            ),
-                          ],
-                        ),
-                        const SpaceWidget(spaceHeight: 24),
-                        ButtonWidget(
-                          onPressed: controller.login,
-                          label: AppStrings.login,
-                          buttonWidth: double.infinity,
-                        ),
-                        const SpaceWidget(spaceHeight: 16),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            const TextWidget(
-                              text: AppStrings.dontHaveAccount,
+                          ),
+                          const SpaceWidget(spaceHeight: 12),
+                          const Center(
+                            child: TextWidget(
+                              text: AppStrings.enterPersonalData,
                               fontColor: AppColors.blueLighter,
                               fontWeight: FontWeight.w400,
                               fontSize: 14,
                             ),
-                            const SpaceWidget(spaceWidth: 4),
-                            TextButtonWidget(
-                              onPressed: () {
-                                Get.toNamed(AppRoutes.registerScreen);
-                              },
-                              text: AppStrings.register,
-                              textColor: AppColors.skyBlue,
-                              fontSize: 14,
-                              fontWeight: FontWeight.w500,
-                              decoration: TextDecoration.underline,
-                              decorationColor: AppColors.skyBlue,
+                          ),
+                          const SpaceWidget(spaceHeight: 28),
+                          const Padding(
+                            padding: EdgeInsets.symmetric(horizontal: 16),
+                            child: TextWidget(
+                              text: AppStrings.email,
+                              fontColor: AppColors.greyDark,
+                              fontWeight: FontWeight.w400,
+                              fontSize: 16,
                             ),
-                          ],
-                        )
-                      ],
+                          ),
+                          const SpaceWidget(spaceHeight: 8),
+                          TextFieldWidget(
+                            controller: controller.emailController,
+                            hintText: 'Enter email',
+                            maxLines: 1,
+                          ),
+                          const SpaceWidget(spaceHeight: 16),
+                          const Padding(
+                            padding: EdgeInsets.symmetric(horizontal: 16),
+                            child: TextWidget(
+                              text: AppStrings.password,
+                              fontColor: AppColors.greyDark,
+                              fontWeight: FontWeight.w400,
+                              fontSize: 16,
+                            ),
+                          ),
+                          const SpaceWidget(spaceHeight: 8),
+                          TextFieldWidget(
+                            controller: controller.passwordController,
+                            hintText: 'Enter password',
+                            maxLines: 1,
+                            suffixIcon: AppIconPath.visibilityOnIcon,
+                          ),
+                          const SpaceWidget(spaceHeight: 24),
+                          // Add Remember Me Checkbox
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Row(
+                                children: [
+                                  SizedBox(
+                                    width: 24,
+                                    height: 24,
+                                    child: Transform.scale(
+                                      scale: 0.9,
+                                      child: Checkbox(
+                                        value: controller.rememberMe.value,
+                                        onChanged: (value) {
+                                          controller.rememberMe.value =
+                                              value ?? false;
+                                        },
+                                        activeColor: AppColors.blueLighter,
+                                        checkColor: AppColors.blueDark,
+                                      ),
+                                    ),
+                                  ),
+                                  const SpaceWidget(spaceWidth: 8),
+                                  const TextWidget(
+                                    text: AppStrings.rememberMe,
+                                    fontColor: AppColors.blueLighter,
+                                    fontWeight: FontWeight.w400,
+                                    fontSize: 12,
+                                  ),
+                                ],
+                              ),
+                              TextButtonWidget(
+                                onPressed: () {
+                                  Get.toNamed(AppRoutes.forgotPasswordScreen);
+                                },
+                                text: AppStrings.forgotPassword,
+                                textColor: AppColors.skyBlue,
+                                fontSize: 12,
+                                fontWeight: FontWeight.w500,
+                                decoration: TextDecoration.underline,
+                                decorationColor: AppColors.skyBlue,
+                              ),
+                            ],
+                          ),
+                          const SpaceWidget(spaceHeight: 24),
+                          ButtonWidget(
+                            onPressed: controller.login,
+                            label: AppStrings.login,
+                            buttonWidth: double.infinity,
+                          ),
+                          const SpaceWidget(spaceHeight: 16),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              const TextWidget(
+                                text: AppStrings.dontHaveAccount,
+                                fontColor: AppColors.blueLighter,
+                                fontWeight: FontWeight.w400,
+                                fontSize: 14,
+                              ),
+                              const SpaceWidget(spaceWidth: 4),
+                              TextButtonWidget(
+                                onPressed: () {
+                                  Get.toNamed(AppRoutes.registerScreen);
+                                },
+                                text: AppStrings.register,
+                                textColor: AppColors.skyBlue,
+                                fontSize: 14,
+                                fontWeight: FontWeight.w500,
+                                decoration: TextDecoration.underline,
+                                decorationColor: AppColors.skyBlue,
+                              ),
+                            ],
+                          )
+                        ],
+                      ),
                     ),
-                  ),
+            ),
           ),
         ),
       ),
